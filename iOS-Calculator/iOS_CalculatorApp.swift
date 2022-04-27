cfeb48e9b4bdceb8abfb8f6a10ba9b5d56f37460
Jordan Haynes <Jordancautious@gmail.com>
2022-04-26 23:12:16 -0400

    Initial Commit

---
+++ iOS-Calculator/iOS_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CalculatorApp.swift
+//  iOS-Calculator
+//
+//  Created by Jordan Haynes on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
